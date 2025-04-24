3b6a315b77abc365d83d76cf8b3fd82a2003b611
victorcreativee <tuyizerevictor@gmail.com>
2025-04-24 17:14:21 +0200

    add-file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module defines a function `concat` that concatenates two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): First string.
+        str2 (str): Second string.
+
+    Returns:
+        str: Concatenated result of str1 and str2.
+    """
+
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module defines a function `to_str` that converts a
+float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: String representation of the float.
+    """
+    return str(n)
